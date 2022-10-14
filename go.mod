83595c4fbacf4a0989d131b25481c8be61482fa1
kan <michael.kan.mail@gmail.com>
2022-10-14 11:50:53 +0200

    go mod tidy

--- go.mod
+++ go.mod
@@ -2,11 +2,12 @@ module github.com/mv-kan/go-oidc-showcase
 
 go 1.19
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	go.uber.org/zap v1.23.0
+)
 
 require (
-	github.com/joho/godotenv v1.4.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	go.uber.org/zap v1.23.0 // indirect
 )
--- openid-provider/cmd/openid-provider/main.go
+++ openid-provider/cmd/openid-provider/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/mv-kan/go-oidc-showcase/openid-provider/pkg/log"
 )
 
 func main() {
 	// test logging
 	log.Info("Testing message")
+	f, err := os.OpenFile("./test.log", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer f.Close()
+	logger := log.New(map[log.LogOutput]io.Writer{
+		log.FileOutput: f,
+	})
+	logger.Info("file log testing message")
 }
